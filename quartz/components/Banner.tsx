8044ec0c465ce6c390b0b7f6d3c948a7970b2839
Alicia Frederick <hink0070@gmail.com>
2024-03-10 01:04:02 -0600

    vault backup: 2024-03-10 01:04:02

--- quartz/components/Banner.tsx
+++ quartz/components/Banner.tsx
@@ -3,12 +3,14 @@ import { classNames } from "../util/lang"
 
 const Banner: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const bannerImage = fileData.frontmatter?.banner
-  const bannerPosition = fileData.frontmatter?.banner_y ?? 0.5
+  const bannerPosition = fileData.frontmatter?.banner_y ?? 50%
   
   if (bannerImage) {
     return (
-        <div class={classNames(displayClass, "obsidian-banner")}>
-            <img class={classNames(displayClass, "banner-image")} src={bannerImage}>
+        <div class={classNames(displayClass, "obsidian-banner-wrapper")}>
+            <div class={classNames(displayClass, "obsidian-banner")}>
+                <img class={classNames(displayClass, "banner-image")} src={bannerImage}/>
+            </div>
         </div>
     )
   } else {
@@ -17,12 +19,14 @@ const Banner: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProp
 }
 
 Banner.css = `
-:root {
-    --banner-height: 250px;
-    --banner-internal-embed-height: 200px;
-    --banner-preview-embed-height: 120px;
+.obsidian-banner .banner-image {
+    position: relative;
+    object-fit: cover;
+    max-width: none;
+    height: 100%;
+    width: 100%;
+    object-position: 50% {bannerPosition};
 }
-
 .obsidian-banner {
     position: absolute;
     top: 0;
@@ -33,16 +37,17 @@ Banner.css = `
     overflow: hidden;
     user-select: none;
 }
-
-.obsidian-banner .banner-image {
-    position: relative;
-    object-fit: cover;
-    max-width: none;
-    height: 100%;
-    width: 100%;
-    object-position: 50% {bannerPosition};
+.obsidian-banner-wrapper{
+    height:250px;
+    width:100%;
+    margin-top:calc(var(--banner-height) - 15px);
+    padding-top:.5em;
 }
-
+html {
+    --banner-height: 250px;
+    --banner-internal-embed-height: 200px;
+    --banner-preview-embed-height: 120px;
+}
 `
 
 export default (() => Banner) satisfies QuartzComponentConstructor
